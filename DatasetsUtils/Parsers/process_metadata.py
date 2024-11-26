cb18dce14a9b3fd00164e944f0ecf0973584e2bf
Nicolas Fabian Buero De La Fuente <nikobuero@hotmail.com>
2024-11-25 23:09:40 -0300

    feat: Nuevo downloader con todo el contexto. Modulo para utils de dataset

---
+++ DatasetsUtils/Parsers/process_metadata.py
@@ -0,0 +1,146 @@
+# Modulo para procesar los archivos de metadata descargados de CKAN.
+# Dados los dos tipos de metadata que descargamos
+#     - metadata_...json: Contiene la metadata de los datasets en formato JSON
+#     - potential_metadata_...ext: Contiene la metadata de los datasets en un formato
+#                                  que no es JSON, como CSVs o XMLs.
+# En ambos casos el formato puede ser engañoso, por ejemplo un .json que en realidad
+# contiene un CSV, o un .csv que contiene un JSON. Por lo que se necesita un proceso
+# de reconocimiento de los archivos para poder procesarlos correctamente.
+# En caso de no poder reconocer el tipo, se manejará como un .txt.
+import os
+import json
+import csv
+import chardet
+# from DatasetsDownloaders.helper import detect_encoding
+
+interest_word = "transparencia"
+# Rutas de directorios
+download_folder = f"DownloadedDatasets/{interest_word}"
+output_directory = f"DatasetsCollection/{interest_word}"
+os.makedirs(output_directory, exist_ok=True)
+
+# Función para detectar encoding
+# Duplicada. TODO: importar de DatasetsUtils.helper
+def detect_encoding(file_path, default_encoding="utf-8"):
+    try:
+        # Intentamos leer los primeros bytes del archivo para detectar el encoding
+        with open(file_path, 'rb') as f:
+            result = chardet.detect(f.read())
+        return result['encoding'] if result['encoding'] else default_encoding
+    except Exception as e:
+        return default_encoding
+
+def recognize_and_process_potential_metadata(file_path):
+    """Reconoce el tipo de archivo potencial y lo procesa adecuadamente."""
+    encoding = detect_encoding(file_path)
+    with open(file_path, "r", encoding=encoding) as file:
+        try:
+            metadata = json.load(file)
+            return "json", metadata
+        except json.JSONDecodeError:
+            try:
+                file.seek(0)  # Reiniciar puntero de lectura
+                csv_reader = csv.reader(file)
+                rows = list(csv_reader)
+                return "csv", rows
+            except Exception:
+                file.seek(0)  # Reiniciar puntero de lectura
+                return "txt", file.read()
+
+def write_file(output_path, content, file_format):
+    """Guarda contenido en el archivo correspondiente según el formato."""
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    if file_format == "json":
+        with open(output_path, "w", encoding="utf-8") as f_out:
+            json.dump(content, f_out, indent=2)
+    elif file_format == "csv":
+        with open(output_path, "w", encoding="utf-8", newline="") as f_out:
+            csv_writer = csv.writer(f_out)
+            csv_writer.writerows(content)
+    elif file_format == "txt":
+        with open(output_path, "w", encoding="utf-8") as f_out:
+            f_out.write(content)
+
+def process_directory(root, dir_name):
+    """Procesa todos los archivos dentro de un directorio específico."""
+    dir_path = os.path.join(root, dir_name)
+    # Directorio de salida, con el id del package como directorio
+    output_dir = os.path.join(output_directory, dir_name)
+    additional_info_path = os.path.join(dir_path, "additional_info.json")
+    
+    # Cargar additional_info.json
+    if not os.path.exists(additional_info_path):
+        print(f"No se encontró additional_info.json en {dir_name}. Saltando.")
+        return
+    
+    with open(additional_info_path, "r", encoding="utf-8") as f:
+        additional_info = json.load(f)
+
+    for filename in os.listdir(dir_path):
+        if "metadata" not in filename:
+            continue
+
+        print (f"   Procesando {filename}...")
+        file_path = os.path.join(dir_path, filename)
+        extension, content = recognize_and_process_potential_metadata(file_path)
+        output_path = os.path.join(output_dir, filename)
+
+        if filename.startswith("metadata_") and filename.endswith(".json"):
+            # Extraer el id del archivo
+            file_id = filename.replace("metadata_", "").replace(".json", "")
+            if extension == "json":
+                # Se mantiene el formato original
+                write_file(output_path, content, "json")
+            elif extension == "csv":
+                # El formato reconocido es CSV, se guarda en formato CSV
+                file_output = output_path.replace(".json", ".csv")
+                write_file(file_output, content, "csv")
+                additional_info["metadata_resources"][file_id]["format"] = "csv"
+            else:
+                # No se reconocio el formato, se guarda como txt
+                file_path = output_path.replace(".json", ".txt")
+                write_file(f"potential_{file_path}", content, "txt")
+                # Mover de metadata_resources a potential_metadata_resources
+                additional_info["potential_metadata_resources"][file_id] = additional_info["metadata_resources"].pop(file_id, None)
+                additional_info["potential_metadata_resources"][file_id]["format"] = "txt"
+                
+                # Remover metadata de la lista de metadata_resources, ya que no es consistente en principio
+                additional_info["metadata_resources"].pop(file_id, None)
+        elif filename.startswith("potential_metadata_"):
+            # Extraer el id del archivo
+            file_id = filename.replace("potential_metadata_", "").split(".")[0]
+            if extension == "json":
+                file_output = os.path.join(output_dir, f"metadata_{file_id}.json")
+                write_file(file_output, content, "json")
+                # Mover de potential_metadata_resources a metadata_resources
+                additional_info["metadata_resources"][file_id] = additional_info["potential_metadata_resources"].pop(file_id, None)
+                additional_info["metadata_resources"][file_id]["format"] = "json"
+                
+                # Remover metadata de la lista de potential_metadata_resources, ya que es consistente. Moverlo a metadata_resources
+                additional_info["potential_metadata_resources"].pop(file_id, None)
+            elif extension == "csv":
+                file_output = os.path.join(output_dir, f"metadata_{file_id}.csv")
+                write_file(file_output, content, "csv")
+                additional_info["metadata_resources"][file_id] = additional_info["potential_metadata_resources"].pop(file_id, None)
+                additional_info["metadata_resources"][file_id]["format"] = "csv"
+                
+                # Remover metadata de la lista de potential_metadata_resources, ya que es consistente. Moverlo a metadata_resources
+                additional_info["potential_metadata_resources"].pop(file_id, None)
+            else:
+                file_output = os.path.join(output_dir, f"metadata_{file_id}.txt")
+                write_file(file_output, content, "txt")
+                additional_info["potential_metadata_resources"][file_id]["format"] = "txt"
+
+    # Guardar additional_info actualizado
+    additional_info_output_path = os.path.join(output_dir, "additional_info.json")
+    write_file(additional_info_output_path, additional_info, "json")
+
+# Procesar archivos
+print("Procesando archivos...")
+
+for root, dirs, _ in os.walk(download_folder):
+    for dir_name in dirs:
+        print(f"# Procesando {dir_name}...")
+        process_directory(root, dir_name)
+
+print("Procesamiento completado.")
---
+++ DatasetsUtils/Parsers/process_tables.py
@@ -0,0 +1,112 @@
+import os
+import chardet
+import pandas as pd
+# from DatasetsUtils.helper import process_csv
+
+interest_word = "transparencia"
+download_folder = f"DownloadedDatasets/{interest_word}"
+
+# Función para detectar encoding
+# Duplicada. TODO: importar de DatasetsUtils.helper
+def detect_encoding(file_path, default_encoding="utf-8"):
+    try:
+        # Intentamos leer los primeros bytes del archivo para detectar el encoding
+        with open(file_path, 'rb') as f:
+            result = chardet.detect(f.read())
+        return result['encoding'] if result['encoding'] else default_encoding
+    except Exception as e:
+        return default_encoding
+
+# Función para procesar un archivo CSV
+# Duplicada. TODO: importar de DatasetsUtils.helper
+def process_csv(file_path, output_path, max_rows=20):
+    # Inicializamos la variable de encoding
+    encoding = 'utf-8'
+    # Intentamos leer el archivo con utf-8
+    try:
+        df = pd.read_csv(file_path, sep=None, engine='python', quotechar='"', encoding='utf-8')
+    except Exception:
+        # Si falla, detectamos el encoding
+        encoding = detect_encoding(file_path)
+        try:
+            df = pd.read_csv(file_path, sep=None, engine='python', quotechar='"', encoding=encoding)
+        except Exception as e:
+            print(f"Error leyendo el archivo {file_path}: {e}")
+            return None, None
+    
+    # Truncar las filas a un máximo de 20
+    # Tomar 20 filas random
+    df_truncated = df.sample(n=min(max_rows, len(df)), random_state=1)
+
+    # Guardar el archivo procesado
+    df_truncated.to_csv(output_path, index=False, encoding='utf-8')
+    
+    print(f"Archivo procesado y truncado: {output_path}")
+    # Retornar las métricas del archivo original
+    return len(df.columns), len(df)
+
+# Procesamiento de CSVs para truncarlos y uniformizar su encoding
+# Ruta al directorio con los CSVs
+output_directory = f"DatasetsCollection/{interest_word}"
+os.makedirs(output_directory, exist_ok=True)
+
+# Variables para las métricas
+total_columns_original = 0
+total_rows_original = 0
+total_cells_original = 0
+
+total_columns_processed = 0
+total_rows_processed = 0
+total_cells_processed = 0
+
+# Procesamiento de los archivos en el directorio de descargas
+print("Procesando archivos...")
+
+for root, dirs, files in os.walk(download_folder):
+  for filename in files:
+    if filename.startswith("table_") and filename.endswith(".csv"):
+      file_path = os.path.join(root, filename)
+      relative_path = os.path.relpath(root, download_folder)
+      output_path = os.path.join(output_directory, relative_path, filename)
+      
+      if os.path.exists(output_path):
+        print("File already exists. Skipping process.")
+        continue
+      
+      # Crear directorio de salida si no existe
+      os.makedirs(os.path.dirname(output_path), exist_ok=True)
+      
+      # Procesar el archivo CSV y obtener métricas del archivo original
+      columns_original, rows_original = process_csv(file_path, output_path)
+      
+      # Si el archivo fue procesado exitosamente
+      if columns_original is not None and rows_original is not None:
+        total_columns_original += columns_original
+        total_rows_original += rows_original
+        total_cells_original += columns_original * rows_original
+
+        # Ahora leemos el archivo procesado para obtener las métricas truncadas
+        columns_processed = columns_original
+        rows_processed = rows_original if rows_original <= 20 else 20
+        
+        total_columns_processed += columns_processed
+        total_rows_processed += rows_processed
+        total_cells_processed += columns_processed * rows_processed
+            
+print("Procesamiento completo.\n")
+
+# Mostrar las métricas de los archivos originales
+print("Métricas de los archivos descargados:")
+print("Cantidad total de archivos descargados:", len([f for root, dirs, files in os.walk(download_folder) for f in files if f.endswith(".csv")]))
+print("Cantidad total de columnas originales:", total_columns_original)
+print("Cantidad total de filas originales:", total_rows_original)
+print("Cantidad total de celdas originales:", total_cells_original)
+
+print("\n")
+
+# Mostrar las métricas de los archivos procesados
+print("Métricas de los archivos procesados:")
+print("Cantidad total de archivos procesados:", len([f for root, dirs, files in os.walk(output_directory) for f in files if f.endswith(".csv")]))
+print("Cantidad total de columnas procesadas:", total_columns_processed)
+print("Cantidad total de filas procesadas:", total_rows_processed)
+print("Cantidad total de celdas procesadas:", total_cells_processed)
